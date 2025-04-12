dbab1034b66a55a8796ca52f4e97b9e1d5e447cd
asilbek <asilbekegamnazarov999@gmail.com>
2025-04-12 21:08:10 +0500

    add

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import images from "../assets/svg"
 
 function About() {
   return (
-    <div></div>
+    <section className='container about flex'>
+      <div>
+        <h1>About</h1>
+        <p>
+          The model offers a framework for discussing problems related to the user's experience, as well as possible ways and means of solving them. Application development begins at the top level (strategy), where the future software product is described quite abstractly from the point of view of the expectations of both users and the customer.
+        </p>
+        <div className='flex'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div>
+        <img src={images.about} alt={images.about} />
+      </div>
+    </section>
   )
 }
 
